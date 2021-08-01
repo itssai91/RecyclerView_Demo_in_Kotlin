18ec8125140154d937ba7c939b584aea5632831f
Saikrishna Panda <itssai91@gmail.com>
2021-08-01 19:17:37 +0530

    additional changes done

--- app/src/main/java/com/infranetstudio/com/recyclerviewdemo/MyAdapter.kt
+++ app/src/main/java/com/infranetstudio/com/recyclerviewdemo/MyAdapter.kt
@@ -4,46 +4,39 @@ import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Adapter
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import org.w3c.dom.Text
 import java.util.zip.Inflater
 
 
-class MyAdapter(val songs: ArrayList<SongList>) : RecyclerView.Adapter<MyAdapter.MyViewHolder>() {
+class MyAdapter(val songs:ArrayList<SongList>): RecyclerView.Adapter<MyAdapter.ViewHolder>() {
 
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-//        return MyViewHolder(
-//            LayoutInflater.from(parent.context).inflate(R.layout.item_view, parent, false)
-//        )
-        val inflate: LayoutInflater? = LayoutInflater.from(parent.context)
-        val view: View = inflate!!.inflate(R.layout.item_view, parent, false)
-        return MyViewHolder(view)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context).inflate(R.layout.item_view, parent, false)
+        )
     }
 
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.title.text = songs[position].getTitle()
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+       holder.title.text = songs[position].getTitle()
         holder.artist.text = songs[position].getArtist()
-        holder.image.setImageResource(songs[position].getImage())
-
-        var color = "#EEEEEE"
-        if (position % 2 == 0) {
-            color = "#CCCCCC"
-        }
-        holder.container.setBackgroundColor(Color.parseColor(color))
+        holder.img.setImageResource(songs[position].getImage())
     }
 
     override fun getItemCount(): Int {
         return songs.size
     }
 
-    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val title = itemView.findViewById<TextView>(R.id.tvItemTitle)
-        val artist = itemView.findViewById<TextView>(R.id.tvItemDesc)
-        val image = itemView.findViewById<ImageView>(R.id.ivImage)
-        val container = itemView.findViewById<LinearLayout>(R.id.container)
+    class ViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
+
+        var title  = itemView.findViewById<TextView>(R.id.tvItemTitle)
+        var artist = itemView.findViewById<TextView>(R.id.tvItemDesc)
+        var img = itemView.findViewById<ImageView>(R.id.ivImage)
 
     }
 }
